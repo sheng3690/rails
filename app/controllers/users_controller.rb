97e1ce858e18b020f78ce8b0980f36a27c0b056b
刘惊坤 <liujkon@gmail.com>
2015-11-23 14:13:41 +0800

    添加用户注册功能

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,19 @@
 #encoding: utf-8
 class UsersController < ApplicationController
   def register
+      @user = User.new
+      render 'register', layout: 'register'
     
   end
 
   def register_confirm
+      @user = User.new params.require(:user).permit(:username,:em    ail,:password,:password_confirmation)
+      if @user.save
+          to_login @user
+          redirect_to root_path
+      else
+          render 'register', layout: 'register'
+      end
     
   end
 
