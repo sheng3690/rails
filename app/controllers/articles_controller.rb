4886ec4e957e9103ae8e8c245875a6a1c5a5d9e7
tanhuazhe <thz@whu.edu.cn>
2015-12-20 16:40:09 -0600

    搭建articles框架

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,44 @@
+class ArticlesController < ApplicationController
+ 
+  def index
+   
+  end
+
+  def new
+    
+  end
+
+  def create
+    
+  end
+
+  def show
+    
+  end
+
+  def edit
+
+  end
+
+  def update
+    
+  end
+
+  def destroy
+    
+  end
+
+  def star
+    
+  end
+
+  protected
+
+  def check_current_user_is_admin
+   
+  end
+
+  def article
+    
+  end
+end
