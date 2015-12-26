a95c79cd6c8edefb1d1a961ec9d5164d653782d2
meganchen15 <megan.w.chen@outlook.com>
2015-12-26 12:25:39 +0800

    完成用户信息设置功能

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,7 +1,9 @@
+#encoding: utf-8
 class BlogsController < ApplicationController
-  
+  before_filter :require_login, only: [:set, :set_userinfo, :upload_img, :set_blog, :update_blog, :change_password, :update_password]
   def index
-    
+    @articles = Article.order('updated_at desc').limit(10)
+    @new_comments = ArticleComment.order('updated_at desc').limit(10)
   end
 
   def set
@@ -9,11 +11,29 @@ class BlogsController < ApplicationController
   end
 
   def set_userinfo
-    
+    user = User.find @current_user.id
+    user.nick_name = params[:user][:nick_name] if params[:user][:nick_name].present?
+    begin
+      if params[:user][:avatar].present?
+        upload_info = upload_picture params[:user][:avatar]
+        user.avatar = "/images/#{upload_info[:real_file_name]}"
+      end
+    rescue UploadException => e
+      flash.now[:error] = e.message
+      render 'set'
+    else
+      if user.save
+        redirect_to set_blogs_path
+      else
+        flash.now[:error] = user.errors.full_messages.first
+        render 'set'
+      end
+    end
   end
 
   def set_blog
-    
+    @blog = BlogInfo.first
+    @blog = BlogInfo.new unless @blog
   end
 
   def update_blog
@@ -21,7 +41,7 @@ class BlogsController < ApplicationController
   end
 
   def about
-    
+    @blog = BlogInfo.first
   end
 
   def change_password
@@ -37,7 +57,12 @@ class BlogsController < ApplicationController
   end
 
   def preview
-    
+    result = {status: false, message: ''}
+    if params[:content]
+      result[:status] = true
+      result[:message] = markdown_parser params[:content]
+    end
+    render json: result.to_json
   end
 
   protected
