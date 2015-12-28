54bdaec347d3197e95a04954074d0889e28c16a6
meganchen15 <megan.w.chen@outlook.com>
2015-12-28 12:34:18 +0800

    完成上传头像功能

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -53,7 +53,19 @@ class BlogsController < ApplicationController
   end
 
   def upload_img
-    
+    @result = {status: false, message: '', text_id: params[:upload][:text_id] || ''}
+    begin
+      if params[:upload].present? && params[:upload][:img].present? && remotipart_submitted?
+        upload_info = upload_picture params[:upload][:img]
+        @result[:status] = true
+        @result[:message] = "![#{upload_info[:file_name]}](/images/#{upload_info[:real_file_name]})"
+      end
+    rescue UploadException => e
+      @result[:message] = e.message
+    end
+    respond_to do |format|
+      format.js
+    end
   end
 
   def preview
@@ -68,6 +80,8 @@ class BlogsController < ApplicationController
   protected
 
   def upload_picture(file)
-    
+    upload_path = File.join Rails.root, 'public/images'
+    upload = SimpleFileupload.new upload_path:upload_path, max_size: 1024*1024*2, type: 'image'
+    upload_info = upload.upload file
   end
 end
